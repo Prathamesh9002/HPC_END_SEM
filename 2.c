2e64adff0c693101fa65b141c1256d1f356214f5
Prathamesh9002 <53111712+Prathamesh9002@users.noreply.github.com>
2021-11-23 15:11:09 +0530

    Update and rename 2.cpp to 2.c

---
+++ 2.c
@@ -0,0 +1,30 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv) {
+  
+    MPI_Init(NULL, NULL);
+
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+   
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    float number;
+    if (world_rank == 0) {
+        number = 1.234567;
+        MPI_Send(&number, 1, MPI_FLOAT, 1, 0, MPI_COMM_WORLD);
+    }
+    else if (world_rank == 1) {
+        MPI_Recv(&number, 1, MPI_FLOAT, 0, 0, MPI_COMM_WORLD,
+            MPI_STATUS_IGNORE);
+        printf("Process 1 received number %f from process 0\n",
+            number);
+    }
+    MPI_Finalize();
+}
